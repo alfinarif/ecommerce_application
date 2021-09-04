73c779ef78b0f50ad3f4695b41a8ea7fd0973455
Ar Alfin Arif <cleavercode@gmail.com>
2021-09-04 14:00:59 +0300

    changes

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,12 +3,5 @@ from django.contrib.sites.shortcuts import get_current_site
 
 
 def homeView(request):
-    list = {
-        'registration': 'https://www.cleverange.com/api/registration/',
-        'login': 'https://www.cleverange.com/api/login/',
-        'logout': 'https://www.cleverange.com/api/logout/'
-    }
-    context = {
-        'lists': list
-    }
-    return render(request, 'index.html', context)
+    
+    return render(request, 'index.html')
